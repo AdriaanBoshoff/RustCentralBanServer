6cc3b5ca7012e96773076dff9fc331b6d049845a
Adriaan Boshoff <adriaanboshoff25@gmail.com>
2021-03-07 19:53:54 +0200

    Check ban request

--- dmWebModule.pas
+++ dmWebModule.pas
@@ -8,6 +8,7 @@ uses
 type
   TWebModuleActions = class(TWebModule)
     procedure WebModule1DefaultHandlerAction(Sender: TObject; Request: TWebRequest; Response: TWebResponse; var Handled: Boolean);
+    procedure WebModuleActionsrustBansAction(Sender: TObject; Request: TWebRequest; Response: TWebResponse; var Handled: Boolean);
   private
     { Private declarations }
   public
@@ -19,6 +20,9 @@ var
 
 implementation
 
+uses
+  uBannedPlayers;
+
 {%CLASSGROUP 'System.Classes.TPersistent'}
 
 {$R *.dfm}
@@ -26,6 +30,28 @@ implementation
 procedure TWebModuleActions.WebModule1DefaultHandlerAction(Sender: TObject; Request: TWebRequest; Response: TWebResponse; var Handled: Boolean);
 begin
   Response.Content := '<html>' + '<head><title>Central Ban Server</title></head>' + '<body>This is the rust central ban server provided with RSMfmx</body>' + '</html>';
+end;
+
+procedure TWebModuleActions.WebModuleActionsrustBansAction(Sender: TObject; Request: TWebRequest; Response: TWebResponse; var Handled: Boolean);
+begin
+  var aSteamID := Request.PathInfo.Replace('/api/rustBans/', '', [rfIgnoreCase]);
+  aSteamID := aSteamID.Replace('/', '', [rfReplaceAll]);
+
+  var data := BannedPlayers.GetBan(aSteamID);
+
+  if data = '' then
+  begin
+    Response.StatusCode := 404;
+    Response.Content := 'Ban does not exist';
+    Handled := True;
+  end
+  else
+  begin
+    Response.StatusCode := 200;
+    Response.Content := data;
+    Response.ContentType := 'application/json';
+    Handled := True;
+  end;
 end;
 
 end.
