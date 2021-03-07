b20c04d894cf517d460cb58ff2af3941bf7f7842
Adriaan Boshoff <adriaanboshoff25@gmail.com>
2021-03-07 19:54:20 +0200

    Save Bans

--- uBannedPlayers.pas
+++ uBannedPlayers.pas
@@ -16,7 +16,11 @@ type
 type
   TBannedPlayers = class
   private
-    BannedPlayers: TDictionary<string, TPlayer>;
+    var
+      BannedPlayers: TDictionary<string, TPlayer>;
+    const
+      BANS_FILE = 'bans.json';
+    procedure SaveBans;
   public
     procedure AddBan(const aSteamID: string; const aReason: WideString; const aExpires: Int64);
     procedure RemoveBan(const aSteamID: string);
@@ -31,16 +35,27 @@ var
 
 implementation
 
+uses
+  uMisc;
+
 { TBannedPlayers }
 
 procedure TBannedPlayers.AddBan(const aSteamID: string; const aReason: WideString; const aExpires: Int64);
 begin
-  var aPlayer: TPlayer;
-  aPlayer.SteamID := aSteamID;
-  aPlayer.Reason := aReason;
-  aPlayer.Expires := aExpires;
+  var pCheckDup: TPlayer;
+  if not BannedPlayers.TryGetValue(aSteamID, pCheckDup) then
+  begin
+    var aPlayer: TPlayer;
+    aPlayer.SteamID := aSteamID;
+    aPlayer.Reason := aReason;
+    aPlayer.Expires := aExpires;
 
-  BannedPlayers.Add(aPlayer.SteamID, aPlayer);
+    BannedPlayers.Add(aPlayer.SteamID, aPlayer);
+    Writeln('Added Ban - ' + aSteamID);
+
+    if aSteamID <> 'test' then
+      SaveBans;
+  end;
 end;
 
 constructor TBannedPlayers.Create;
@@ -107,6 +122,47 @@ end;
 procedure TBannedPlayers.RemoveBan(const aSteamID: string);
 begin
   BannedPlayers.Remove(aSteamID);
+  Writeln('Removed ban - ' + aSteamID);
+end;
+
+procedure TBannedPlayers.SaveBans;
+begin
+  var StringWriter := TStringWriter.Create;
+  var Writer := TJsonTextWriter.Create(StringWriter);
+  try
+    Writer.Formatting := TJsonFormatting.Indented;
+    try
+      Writer.WriteStartArray;
+      for var aPlayer in BannedPlayers.Values do
+      begin
+        Writer.WriteStartObject;
+        Writer.WritePropertyName('steamId');
+        Writer.WriteValue(aPlayer.SteamID);
+        Writer.WritePropertyName('reason');
+        Writer.WriteValue(aPlayer.Reason);
+        Writer.WritePropertyName('expiryDate');
+        Writer.WriteValue(aPlayer.Expires);
+        Writer.WriteEndObject;
+      end;
+      Writer.WriteEndArray;
+
+      SaveToFile(StringWriter.ToString, ExtractFilePath(ParamStr(0)) + BANS_FILE);
+
+      Writeln('Saved ' + BannedPlayers.Count.ToString + ' bans');
+    except
+      on E: Exception do
+      begin
+        Writeln('============================');
+        Writeln('ERROR Savings Bans:');
+        Writeln('Procedure        : SaveBans');
+        Writeln('Reason           : ' + E.Message);
+        Writeln('============================');
+      end;
+    end;
+  finally
+    Writer.Free;
+    StringWriter.Free;
+  end;
 end;
 
 end.
